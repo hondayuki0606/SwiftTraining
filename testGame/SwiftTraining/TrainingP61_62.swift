f128a513adf797537f3bf2a8e6d23da8a843cc26
honda <hondayuki0606@yahoo.co.jp>
2020-10-17 00:53:59 +0900

    暗黙的にアンラップされたOptional＜Wrapped＞型 P61-62

---
+++ testGame/SwiftTraining/TrainingP61_62.swift
@@ -0,0 +1,33 @@
+//
+//  TrainingP61_62.swift
+//  testGame
+//
+//  Created by 本田尚行 on 2020/10/17.
+//  Copyright © 2020 本田尚行. All rights reserved.
+//
+
+import Foundation
+
+func 暗黙的にアンラップされたOptionalWrapped型_61(){
+//    Optional＜Wrapped＞型にはWrapped?型と表記する糖衣構文がありました。
+//    Optional＜Wrapped＞型にはもう一つ、Wrapped！と表記する糖衣構文が
+//    用意されています。この糖衣構文によって生成されたOptional＜Wrapped＞型の値は、
+//    値へのアクセス次に自動的に強制アンラップを行うため、暗黙的にアンラップされた
+//    Optional＜Wrapped＞型と言います。
+//    通常のOptional＜Wrapped＞型であるWrapped型？も、暗黙的にアンラップされ
+//    たOptional＜Wrapped＞型であるWrapped型！も、同じOptional＜Wrapped＞型であるため、
+//    どちらで宣言された定数や変数にも互いの値が代入できます。
+    let a:String? = "a"
+    let b:String! = "b"
+    
+    print(type(of:a))
+    print(type(of:b))
+    
+    var c: String! = a
+    var d: String! = b
+    
+    //実行結果
+//    Optional＜String＞
+//    Optional＜String＞
+    
+}
